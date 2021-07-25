2b87c3d03d12119ee780ddeefea16ff7e83fa309
Azami Base <azamibase@gmail.com>
2021-07-25 15:15:23 -0400

    create VS project

---
+++ VRChat Hue/VRChat Hue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRChat_Hue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+
+            Console.ReadKey();
+        }
+    }
+}
